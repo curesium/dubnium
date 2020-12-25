2e2e9fb939eac8dfe13e4d7728027a1658237fd8
Thijs Boom <twboom@hotmail.nl>
2020-12-25 01:10:10 +0100

    Got the search function partially working

--- #dubnium/js/search.js
+++ #dubnium/js/search.js
@@ -1,10 +1,16 @@
 const url = 'https://dubnium.curesium.com/assets/data/intervals.json';
-var songList;
+var intervals;
+var found;
 
 fetch(url).then(
     function(u){ return u.json();}
   ).then(
     function(json){
-      songList = json;
+      intervals = json;
     }
   )
+
+function search() {
+  found = intervals.find(element => element.first == 1);
+  console.log(found)
+}
